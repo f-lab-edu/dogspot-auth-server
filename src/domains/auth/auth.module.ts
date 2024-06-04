8e1c14fc0bb2d48b842eaff178a30ef44fb26ffd
문준호 <wngh46280@gmail.com>
2024-06-04 21:00:23 +0900

    feat: jwt 회원 가입 & jwt 토큰 및 리프레시 토큰 구현

---
+++ src/domains/auth/auth.module.ts
@@ -0,0 +1,45 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmExModule } from 'src/core/typeorm-ex.module';
+import { UserRepository } from '../user/repositories/user.repository';
+import { UserModule } from './../user/user.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt/jwt.strategy';
+import { JwtRefreshTokenStrategy } from './jwt/refresh-token.strategy';
+import { RefTokenRepository } from './repositories/ref-token.repository';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? 'env.dev' : 'env.prod',
+    }),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+          expiresIn: '2h',
+        },
+      }),
+    }),
+    TypeOrmExModule.forCustomRepository([UserRepository, RefTokenRepository]),
+    PassportModule,
+    forwardRef(() => UserModule),
+    // RedisModule.forRoot({
+    //   readyLog: true,
+    //   config: {
+    //     host: process.env.REDIS_HOST,
+    //     // host : 'www.reptimate.store',
+    //     port: 6379,
+    //     // password: 'foobared'
+    //   },
+    // }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, JwtRefreshTokenStrategy],
+  exports: [AuthService, PassportModule, JwtRefreshTokenStrategy], // ?: JwtStrategy, PassportModule, JwtRefreshTokenStrategy은 왜 export를 하는건지
+})
+export class AuthModule {}
