8711358b50c47e71abd9edc8ff21d397b3725c28
문준호 <wngh46280@gmail.com>
2024-06-23 19:47:06 +0900

    fix: 포트 번호 3001번으로 변경

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   // cors 설정x
   app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(3001);
 
   // 어플리케이션이 클라이언트의 요청처리가 가능한 상태가 되었을때 pm2에 ready신호를 보내기 (process.send 함수는 pm2로 실행되었을 경우에만 존재하여 분기처리를 했다)
   if (process.send) {
